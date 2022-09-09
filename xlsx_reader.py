8fac05637819a6c1369f6623d245b4763f4fc8a0
93simon <93simonster@gmail.com>
2022-09-09 11:01:49 +0200

    working?

--- xlsx_reader.py
+++ xlsx_reader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from utils import ann_folder, json_folder, text_folder, get_filestar_name, read_txt, get_filename_no_ext
+from pathlib import Path
 
 
 class ReviewedAnnotation:
@@ -14,28 +15,38 @@ class ReviewedAnnotation:
             self.valid = False
 
 
-def new_ann():
-    pass
-
 def main():
     df = pd.read_excel("out - Copy.xlsx")
     for ind in df.index:
+        print("-"*10)
         r = ReviewedAnnotation(df['filename'][ind], df['testo'][ind], df['entità rilevata'][ind], df["valido"][ind])
         if r.valid:
-            annotations_for_current_file = ann_folder.glob(f'**/{get_filestar_name(r.txt_filename)}')
-            for file in annotations_for_current_file:
-                content = read_txt(file)
-                if r.domain in content:
-                    print("pass")
+            annotations_for_current_file = list(ann_folder.glob(f'**/{get_filestar_name(r.txt_filename)}'))
+            ann_numbers = [] # numeri dei sottofile con singole annotazioni
+            domains_for_current_file = []
+            for f in annotations_for_current_file:
+                print(f"\ncurrent: {f}")
+                ann_numbers.append(get_filename_no_ext(f)[-1])
+                content = read_txt(f)
+                content = content.split("\t")[-1].strip()
+                domains_for_current_file.append(content)
+                print(f"tabella excel - {r.domain} vs annotazione - {content}")
+                if r.domain in set(domains_for_current_file):
+                    print(f"breaking on {r.txt_filename}, {r.domain} already in {set(domains_for_current_file)}")
+                    break
+                next_ann_num = int(max(ann_numbers)) + 1
+                next_ann_name = get_filename_no_ext(r.txt_filename)[:-1] + str(next_ann_num)
+
+                if not Path(f"ann/{next_ann_name}.ann").is_file():
+                    print(f"ann {next_ann_name} not exist, creating")
+                    with open(f"ann/{next_ann_name}.ann", "a", encoding="utf-8") as ann:
+                        print(f"C1		{r.domain}\n")
+                        ann.write(f"C1		{r.domain}\n")
+                    with open(f"text/{next_ann_name}.txt", "a", encoding="utf-8") as txt:
+                        print(r.text)
+                        txt.write(r.text)
                 else:
-                    ann_numbers = []
-                    for f in ann_folder.glob(f'**/{get_filestar_name(file)}'):
-                        ann_numbers.append(get_filename_no_ext(f)[-1])
-            if ann_numbers:
-                next_ann_for_file = get_filename_no_ext(r.txt_filename)[:-1] + str(int(max(ann_numbers))+1)
-                with open(f"ann/new/{next_ann_for_file}.ann", "a", encoding="utf-8") as ann:
-                    tax_count = 1
-                    ann.write(f"C{tax_count}		{r.domain}\n")
+                    print(f"ann {next_ann_name} already exists")
 
 
 if __name__ == "__main__":
